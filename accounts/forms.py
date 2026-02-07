2102e2c8826be12ea965ebfce41044a188e2ee74
Ehab Emad <ehabemadaboshady@gmail.com>
2026-02-07 10:53:50 +0200

    Done account

---
+++ accounts/forms.py
@@ -0,0 +1,29 @@
+from django import forms 
+from .models import *
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder' : 'Enter Password' ,
+        'class':'form-control',
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder' : 'Confirm Password'
+    }))
+    class Meta:
+        model = Account
+        fields = ['first_name' , 'last_name' , 'phone_number' , 'email' , 'password']
+    
+    def __init__(self,*args,**kwargs):
+        super(RegistrationForm , self).__init__(*args,**kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter last Name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Numper'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter Email Address'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+        
+    def clean(self):
+        cleaned_date = super(RegistrationForm , self).clean()
+        password = cleaned_date.get('password')
+        confirm_password = cleaned_date.get('confirm_password')
+        return super().clean()
