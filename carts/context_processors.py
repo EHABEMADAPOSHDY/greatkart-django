dbfda0d215c9db76c5efc4d68461775eb74275d9
Ehab Emad <ehabemadaboshady@gmail.com>
2026-02-18 11:50:16 +0200

    authenticated

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -1,13 +1,25 @@
-from .models import *
+from .models import Cart, CartItem
 from django.db.models import Sum
-from .views  import _cart_id
+from .views import _cart_id
+
 def counter(request):
     cart_count = 0
     try:
-        cart = Cart.objects.get(cart_id=_cart_id(request))
-        cart_count = CartItem.objects.filter(cart=cart).aggregate(
-            total=Sum('quantity')
-        )['total'] or 0
+        if request.user.is_authenticated:
+            cart_count = CartItem.objects.filter(
+                user=request.user
+            ).aggregate(
+                total=Sum('quantity')
+            )['total'] or 0
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_count = CartItem.objects.filter(
+                cart=cart
+            ).aggregate(
+                total=Sum('quantity')
+            )['total'] or 0
+
     except Cart.DoesNotExist:
-        pass
+        cart_count = 0
+
     return {'cart_count': cart_count}
